48a2fc7ba340e10a99d64a6d7655c0a36ee00962
Jordan <climbjn189@gmail.com>
2020-03-08 19:38:55 -0400

    Cleaned up

--- randomImage.py
+++ randomImage.py
@@ -1,7 +1,6 @@
 from PIL import Image
 import numpy as np
 from datetime import datetime
-import webbrowser
 
 #Width and height for the image
 w, h = 512, 512
@@ -9,4 +8,3 @@ data = np.random.randint(256, size=(h, w, 3), dtype=np.uint8)
 img = Image.fromarray(data, "RGB")
 todayFile = "images/" + str(datetime.today().strftime("%m-%d-%Y")) + ".png"
 img.save(todayFile)
-#webbrowser.open(todayFile)
