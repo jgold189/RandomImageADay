d4f3849412c56ddd8e86d94c3e273839abadbd32
Jordan <43799218+jgold189@users.noreply.github.com>
2020-08-25 12:53:48 -0400

    Quick comment

--- run.sh
+++ run.sh
@@ -8,4 +8,5 @@ if hash python3>/dev/null; then
 else
 	python randomImage.py
 fi
+#Real hacky command to have a shell script display an image from a cron job
 firefox --new-window file:$(dirname $0)/images/$(date +"%m-%d-%Y").png
